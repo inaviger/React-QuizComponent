2fa48109819b5c5d724dbd5d0dce3feb1ec8b3ab
Istvan Lovas <istvan.lovas@crowdhouse.ch>
2018-12-12 15:38:13 +0100

    Completed the module

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class QuizEnd extends Component {
   handleResetClick() {
     this.props.resetClickHandler();
-    this.setState({ quiz_position: 1 })
   }
 
   render() {
